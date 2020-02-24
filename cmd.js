6ffc6ab656a7351065753d76bb70651c807498df
tywanda <TYWANDAHARMON0328@GMAIL.COM>
2020-02-24 16:07:52 -0500

    EXwait

--- cmd.js
+++ cmd.js
@@ -1,3 +1,8 @@
-module.exports = function candy() {
+module.exports = function candy()  { 
+    setTimeout(function(){ 
+       console.log('heart');
+     }, 3000);
+    
     console.log('candy');
 }
+
