062beb72ec2c977ea2f4bf876f3f66d53f5abee1
tywanda <TYWANDAHARMON0328@GMAIL.COM>
2020-02-28 14:42:17 -0500

    added comment

--- ServerHW/serverPart2.js
+++ ServerHW/serverPart2.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+// edited and added "localhost:8080/about" you server a html web page.
+
 const server = http.createServer((req,res) => {
     if (req.url === '/about')
    
