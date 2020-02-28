9db884188a7e7f2d441a95243b536984bebcccd2
tywanda <TYWANDAHARMON0328@GMAIL.COM>
2020-02-28 13:48:29 -0500

    server part1 done

---
+++ ServerHW/basicServer.js
@@ -0,0 +1,12 @@
+// Create basic Server
+
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+   
+        res.write("<h1>Hello, World!</h1>");
+        res.end();
+    })
+   
+server.listen(2000);
+console.log("basic server setup");
